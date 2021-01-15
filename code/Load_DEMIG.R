fabdbcfe518f4cbf7f1d0ff6531d12fe0e246f14
FabianFox <fabianguelzau@web.de>
2021-01-15 18:52:34 +0100

    demig long

--- code/Load_DEMIG.R
+++ code/Load_DEMIG.R
@@ -17,7 +17,9 @@ demig.df <- import("./data/DEMIG Visa/DEMIG VISA Database_version 1.4.xlsx",
          nationality = `nationality_of_traveller`,
          destination_iso3 = un_3_digit_code_3,
          nationality_iso3 = un_3_digit_code_7,
-         num_range("x", 1989:2014)) %>%
+         policy_measure,
+         num_range("x", 1993:2013)) %>%
+  filter(policy_measure == "Visa") %>%
   # wrong iso3 codes
   mutate(across(c(destination_iso3, nationality_iso3), 
                 ~if_else(. == "ROM", "ROU", 
@@ -29,6 +31,8 @@ demig.df <- import("./data/DEMIG Visa/DEMIG VISA Database_version 1.4.xlsx",
     nationality_iso3 %in% unique(destination_iso3),
     destination_iso3 %in% unique(nationality_iso3))
 
+# Limit DEMIG Visa to independent states
+### ------------------------------------------------------------------------###
 # Gleditsch & Ward (2007) Independent states
 # Load
 data(gwstates)
@@ -43,4 +47,13 @@ gwstates.df <- gwstates %>%
 
 # Limit DEMIG Visa to independent states (gwstates.df)
 demig.df <- demig.df %>%
-  filter(destination_iso3 %in% gwstates.df$iso3c)
+  filter(destination_iso3 %in% gwstates.df$iso3c,
+         nationality_iso3 %in% gwstates.df$iso3c)
+
+# 
+### ------------------------------------------------------------------------###
+demig_long.df <- demig.df %>%
+  pivot_longer(starts_with("x"), names_to = "year", values_to = "visa_restriction") %>%
+  mutate(year = strtoi(str_extract(year, "[:digit:]{4}")),
+         visa_restriction = if_else(destination_iso3 == nationality_iso3, 0, 
+                                    visa_restriction))
