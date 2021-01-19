faa2127e3c723abbc5b66b650a69972ab68ea884
FabianFox <fabianguelzau@web.de>
2021-01-19 17:11:57 +0100

    data cleaning, missing values

--- code/Load_DEMIG.R
+++ code/Load_DEMIG.R
@@ -2,12 +2,15 @@
 # Data from https://www.migrationinstitute.org/data/demig-data/demig-visa-data
 
 # Load/install packages
-### ------------------------------------------------------------------------###
+### ------------------------------------------------------------------------ ###
 if (!require("xfun")) install.packages("xfun")
 pkg_attach2("tidyverse", "lubridate","rio", "janitor", "countrycode", "states")
 
+# Issues
+# Check country codes; so far multiple erroneous entries
+
 # Load data
-### ------------------------------------------------------------------------###
+### ------------------------------------------------------------------------ ###
 # DEMIG Visa
 # Load
 demig.df <- import("./data/DEMIG Visa/DEMIG VISA Database_version 1.4.xlsx",
@@ -24,15 +27,30 @@ demig.df <- import("./data/DEMIG Visa/DEMIG VISA Database_version 1.4.xlsx",
   mutate(across(c(destination_iso3, nationality_iso3), 
                 ~if_else(. == "ROM", "ROU", 
                          if_else(. == "DMA", "DOM", 
-                                 .)))) %>%
-  # missing: French West Indies
-  filter(
+                                 .))))
+
+# Deal with erroneous entries of Japan and Lesotho (i.e. nationality != nationality_iso3)
+# Wrong entries start in row nationality == "Northern Mariana Islands"
+# Use donor rows to correct errors (here: AFG)
+demig.df[demig.df$destination_iso3 %in% c("JPN", "LSO"),]$nationality_iso3 <- 
+  demig.df[demig.df$destination_iso3 %in% "AFG",][
+    match(demig.df[demig.df$destination_iso3 %in% c("JPN", "LSO"),]$nationality, 
+          demig.df[demig.df$destination_iso3 %in% "AFG",]$nationality), 
+  ]$nationality_iso3
+
+# Limit DEMIG Visa to countries that report as sending and receiving country
+demig.df <- demig.df %>%
+    filter(
+    # Vietnam, Rep of (South Vietnam) and Vietnam, Socialist Rep of the same iso3 code
+    !destination == "Vietnam, Rep of (South Vietnam)",
+    # missing: French West Indies
     !is.na(destination_iso3),
+    # both sending and receiving reports
     nationality_iso3 %in% unique(destination_iso3),
     destination_iso3 %in% unique(nationality_iso3))
 
 # Limit DEMIG Visa to independent states
-### ------------------------------------------------------------------------###
+### ------------------------------------------------------------------------ ###
 # Gleditsch & Ward (2007) Independent states
 # Load
 data(gwstates)
@@ -50,8 +68,25 @@ demig.df <- demig.df %>%
   filter(destination_iso3 %in% gwstates.df$iso3c,
          nationality_iso3 %in% gwstates.df$iso3c)
 
-# 
-### ------------------------------------------------------------------------###
+# Remove countries with excessive missing values
+### ------------------------------------------------------------------------ ###
+# Countries with many missing values
+missing <- demig.df %>%
+  rowwise() %>%
+  mutate(row_missing = sum(is.na(c_across(num_range("x", 1993:2013))))) %>%
+  filter(row_missing > 0) %>%
+  count(destination_iso3) %>%
+  filter(n == 171)
+
+# Remove counties with multiple missing values
+demig.df <- demig.df %>%
+  filter(!destination_iso3 %in% missing$destination_iso3,
+         !nationality_iso3 %in% missing$destination_iso3)
+
+# Second stage missing
+
+# Long format 
+### ------------------------------------------------------------------------ ###
 demig_long.df <- demig.df %>%
   pivot_longer(starts_with("x"), names_to = "year", values_to = "visa_restriction") %>%
   mutate(year = strtoi(str_extract(year, "[:digit:]{4}")),
