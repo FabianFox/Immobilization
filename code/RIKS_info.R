6b86142dcef4f20388125946100dd3cfc94007cf
FabianFox <fabianguelzau@web.de>
2021-01-25 15:48:03 +0100

    clean table column

--- code/RIKS_info.R
+++ code/RIKS_info.R
@@ -2,7 +2,7 @@
 
 # Load/install packages
 if (!require("xfun")) install.packages("xfun")
-pkg_attach2("tidyverse", "rvest", "rio", "docxtractr")
+pkg_attach2("tidyverse", "rvest", "rio", "docxtractr", "countrycode")
 
 # All regional integration entities in RIKS
 reg_int.df <- read_docx("./data/Regional Integration/AgreementList_August2020_0.docx") %>%
@@ -46,6 +46,66 @@ reg_missing.df <- reg_int.df %>%
 reg_int.df <- reg_int.df %>%
   rows_update(reg_missing.df %>%
                 select(code, tables), by = "code")
+
+# Load data
+# export(reg_int.df, "./data/Regional Integration/RegMembership.rds")
+reg_int.df <- import("./data/Regional Integration/RegMembership.rds")
+
+# Empty tables
+reg_int.df <- reg_int.df %>%
+  mutate(empty_table = map(tables, ~dim(.x) %>%
+                             .[1] == 0)) %>%
+  filter(empty_table == FALSE)
+
+# Clean data
+reg_int.df <- reg_int.df %>%
+  mutate(
+    # lower case variable names
+    tables = map(tables, ~clean_names(.x)),
+    # clean character strings
+         tables = map(tables, ~.x %>%
+                        mutate(range = qdap::bracketX(range))),
+    # separate joined - left
+         tables = map(tables, ~.x %>%
+                        separate(range, into = c("joined", "current_status")) %>%
+                        # turn character into numeric
+                        mutate(current_status = str_replace(current_status, "Present", "2020"),
+                               across(c("joined", "current_status"), ~strtoi(.x)),
+                               # iso3 code (missing: Sahrawi Arab Dem. Rep. / Serbia-Montenegro / Czechoslovakia / Yugoslavia)
+                               country_iso3 = countrycode(country, "country.name.en", "iso3c", 
+                                                          custom_match = c("Yemen Arab Rep." = "YEM",
+                                                                           "Central African Rep." = "CAF",
+                                                                           "Kosovo" = "XKX")))))
+
+# Data from Czaika et al. (2018) "The Global Evolution of Travel Regimes"
+reg_select_int.df <- tribble(
+  ~code, ~name,
+  "AFTA", "ASEAN Free Trade Area", 
+  "APTA", "Asia Pacific Trade Agreement", 
+  "CACM", "Central American Common Market",
+  "CAN", "Andean Community",
+  "CARICOM", "Caribbean Community",
+  "CEFTA", "Central European Free Trade Agreement",
+  "CEMAC", "Economic and Monetary Community of Central Africa",
+  "CIS", "Commonwealth of Independent States Free Trade Agreement",
+  "COMESA", "Common Market for Eastern and Southern Africa",
+  "EAC", "East African Community",
+  "ECOWAS", "Economic Community of West African States",
+  "EFTA", "European Free Trade Association",
+  "EU", "European Union",
+  "GCC", "Gulf Cooperation Council",
+  "LAIA", "Latin American Integration Association",
+  "MERCOSUR", "Mercado Común del Sur — Common market of the South",
+  "NAFTA", "North American Free Trade Agreement",
+  "PAFTA", "Pan-Arab Free Trade Area",
+  "SACU", "Southern African Customs Union",
+  "SADC", "Southern African Development Community",
+  "SAFTA", "South Asian Free Trade Agreement")
+
+# AFTA (included as ASEAN), APTA 
+reg_select_int.df[which(!reg_select_int.df$code %in% reg_int.df$code),]$code
+
+# Add missing economic blocs included in Czaika et al. (2018)
+# APTA, PAFTA, SAFTA
   
 # Export
-rio::export(reg_int.df, "./data/Regional Integration/RegMembership.rds")
