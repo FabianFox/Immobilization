9352ef2879d9799ca4ddb4242a73e04ac147ffee
FabianFox <fabianguelzau@web.de>
2021-01-28 17:02:59 +0100

    linpolate

--- code/AdditionalVariables.R
+++ code/AdditionalVariables.R
@@ -4,7 +4,10 @@
 ### ------------------------------------------------------------------------ ###
 if (!require("xfun")) install.packages("xfun")
 pkg_attach2("tidyverse", "lubridate","rio", "janitor", "countrycode", "states", 
-            "pwt10", "wbstats")
+            "pwt10", "wbstats", "zoo")
+
+# DEMIG Visa data (from: Load_DEMIG.R)
+demig.df <- import("./data/DEMIG Visa/DEMIG_1993-2013.rds")
 
 # Penn World House 10.0
 # Load
@@ -13,10 +16,11 @@ data("pwt10.0")
 # Limit
 pwt.df <- pwt10.0 %>%
   filter(between(year, 1993, 2013)) %>%
-  select("country_iso3" = isocode, year, "gdp" = rgdpo)
+  select("country_iso3" = isocode, year, "gdp" = rgdpo) %>%
+  filter(country_iso3 %in% unique(demig.df$destination_iso3))
 
-# 
-visa.df <- visa.df %>%
+# Join to DEMIG Visa
+visa.df <- demig.df %>%
   left_join(y = pwt.df, by = c("destination_iso3" = "country_iso3", "year"))
 
 # World Bank Data
@@ -25,6 +29,12 @@ wb.info <- wb_data(country = unique(visa.df$destination_iso3),
                    start_date = 1993, end_date = 2013, return_wide = TRUE) %>%
   select("country_iso3" = iso3c, "year" = date, "gdp_cons" = NY.GDP.PCAP.CD, "gdp_ppp" = NY.GDP.PCAP.PP.CD)
 
-# 
-visa.df <- visa.df %>%
+# Join to DEMIG Visa
+visa.df <- demig %>%
   left_join(y = wb.info, by = c("destination_iso3" = "country_iso3", "year"))
+
+# Interpolate missing values
+### ------------------------------------------------------------------------ ###
+wb.info <- wb.info %>%
+  arrange(country_iso3, year) %>%
+  mutate(ip.gdp = na.approx(gdp_ppp, rule = 2)) # maxgap = 4
